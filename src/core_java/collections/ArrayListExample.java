a6eea604a85266ff467f5d9ffd7f6c7d3bf35389
Prakash Salapu <prakash.salapu@gmail.com>
2026-04-02 23:31:39 +0530

    update settings

--- src/core_java/collections/ArrayListExample.java
+++ src/core_java/collections/ArrayListExample.java
@@ -1,5 +1,6 @@
 public class ArrayListExample {
     public static void main(String[] args) {
         System.out.println("Welcome to collections");
+        System.out.println("Welcome to java");
     }
 }
