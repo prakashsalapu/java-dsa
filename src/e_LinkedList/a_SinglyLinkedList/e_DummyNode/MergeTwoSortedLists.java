2cf7a57fa7ed0ba280eabc150d95f7e5d215fcba
Bhanu Prakash Salapu <bhanuprakash8305@gmail.com>
2026-07-04 17:10:33 +0530

    Add commented brute-force method for merging lists
    Commented out brute-force approach using ArrayList for merging lists.

--- src/e_LinkedList/a_SinglyLinkedList/e_DummyNode/MergeTwoSortedLists.java
+++ src/e_LinkedList/a_SinglyLinkedList/e_DummyNode/MergeTwoSortedLists.java
@@ -12,6 +12,35 @@ public class MergeTwoSortedLists {
 
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        // Brute -  ArrayList
+    /*
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        while(list1 != null){
+            arr.add(list1.val);
+            list1 = list1.next;
+        }
+
+        while(list2 != null){
+            arr.add(list2.val);
+            list2 = list2.next;
+        }
+        
+        Collections.sort(arr);
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        
+        for(int i=0; i<arr.size();  i++){
+            curr.next = new ListNode(arr.get(i));
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    */
+
+        // Optimal - Dummy 
+
         if(list1 == null && list2 == null ) return null;
         if(list1 == null) return list2;
         if(list2 == null) return list1;
